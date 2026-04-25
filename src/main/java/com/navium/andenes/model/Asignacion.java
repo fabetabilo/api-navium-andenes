c9b863680d56751a9839ceecea5423c4402d029c
fabetabilo <fab.tabilo@duocuc.cl>
2026-04-24 21:25:19 -0400

    add: repository Anden, Asignacion

--- src/main/java/com/navium/andenes/model/Asignacion.java
+++ src/main/java/com/navium/andenes/model/Asignacion.java
@@ -2,6 +2,12 @@ package com.navium.andenes.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,17 +16,31 @@ import lombok.Setter;
 /**
  * Asignacion representa el historial de {@link Anden}
  */
+@Entity
+@Table(name = "asignaciones")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Asignacion {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private Long andenId;
+
+    @Column(nullable = false, length = 10)
     private String patenteTransporte;
+
+    @Column(nullable = false)
     private Long contenedorId;
+
+    @Column(nullable = false)
     private LocalDateTime horaInicio;
+
+    @Column(nullable = true)
     private LocalDateTime horaFin; // null si esta activa, temporal!!
     
 }
