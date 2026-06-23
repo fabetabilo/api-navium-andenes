2c733d259cfa729fd8f935c825a43fc0dcab71d9
fabetabilo <fab.tabilo@duocuc.cl>
2026-06-23 01:05:07 -0400

    fix: contexto spring test v2

--- src/test/java/com/navium/andenes/ApiAndenesApplicationTests.java
+++ src/test/java/com/navium/andenes/ApiAndenesApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Requiere conexion a base de datos; no aplica en CI")
 @SpringBootTest
 class ApiAndenesApplicationTests {
 
-	@Disabled("Requiere conexion a base de datos; no aplica en CI")
 	@Test
 	void contextLoads() {
 	}
